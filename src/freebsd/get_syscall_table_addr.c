4d1f02ca954a36ddb6daeb1a3f1a3ffa2f5b57a6
Rafael Santiago <voidbrainvoid@gmail.com>
2017-12-08 11:29:16 +0100

    Adding copyright disclaimer

--- src/freebsd/get_syscall_table_addr.c
+++ src/freebsd/get_syscall_table_addr.c
@@ -1,3 +1,10 @@
+/*
+ *                                Copyright (C) 2017 by Rafael Santiago
+ *
+ * This is a free software. You can redistribute it and/or modify under
+ * the terms of the GNU General Public License version 2.
+ *
+ */
 #include <freebsd/get_syscall_table_addr.h>
 #include <sys/sysent.h>
 
