f01b01c816e7dce216d8582a32061c3e6b07e72b
Rafael Santiago <voidbrainvoid@gmail.com>
2017-12-09 12:57:43 +0100

    K.i.s.s.ing a little more ;)

--- src/test.c
+++ src/test.c
@@ -8,12 +8,7 @@
 #include <cutest/src/kutest.h>
 #include <kook.h>
 
-#define KOOK_TEST_FILE_PATH "kook.paragaricutirimirruaru.test.txt"
-#define KOOK_TEST_DIRNAME "kook-test"
-
 static void *original_mkdir = NULL;
-
-static int hook_callnr = 0;
 
 KUTE_DECLARE_TEST_CASE(kook_test_monkey);
 KUTE_DECLARE_TEST_CASE(hook_test);
@@ -43,36 +38,18 @@ KUTE_MAIN(kook_test_monkey);
 static int dummy_mkdir(struct thread *td, void *args);
 
 KUTE_TEST_CASE(hook_test)
-    struct mkdir_args args;
     KUTE_ASSERT(kook(SYS_mkdir, dummy_mkdir, &original_mkdir) == 0);
     KUTE_ASSERT(original_mkdir == (void *)sys_mkdir);
     KUTE_ASSERT((sy_call_t *)dummy_mkdir == sysent[SYS_mkdir].sy_call);
-    // INFO(Rafael): Ascertaining it.
-    args.path = KOOK_TEST_DIRNAME;
-    args.mode = 0644;
-    KUTE_ASSERT(sysent[SYS_mkdir].sy_call(curthread, &args) == 0);
-    KUTE_ASSERT(hook_callnr == 1);
-    KUTE_ASSERT(kern_rmdirat(curthread, AT_FDCWD, KOOK_TEST_DIRNAME, UIO_SYSSPACE) == 0);
 KUTE_TEST_CASE_END
 
 KUTE_TEST_CASE(unhook_test)
-    struct mkdir_args args;
     KUTE_ASSERT(kook(SYS_mkdir, original_mkdir, NULL) == 0);
     KUTE_ASSERT(original_mkdir == (void *)sysent[SYS_mkdir].sy_call);
-    // INFO(Rafael): Anyway, let's make sure.
-    args.path = KOOK_TEST_DIRNAME;
-    args.mode = 0644;
-    KUTE_ASSERT(sysent[SYS_mkdir].sy_call(curthread, &args) == 0);
-    KUTE_ASSERT(hook_callnr == 1);
-    KUTE_ASSERT(kern_rmdirat(curthread, AT_FDCWD, KOOK_TEST_DIRNAME, UIO_SYSSPACE) == 0);
 KUTE_TEST_CASE_END
 
 static int dummy_mkdir(struct thread *td, void *args) {
-    hook_callnr++;
     return sys_mkdir(td, args);
 }
 
 #endif
-
-#undef KOOK_TEST_FILE_PATH
-#undef KOOK_TEST_DIRNAME
